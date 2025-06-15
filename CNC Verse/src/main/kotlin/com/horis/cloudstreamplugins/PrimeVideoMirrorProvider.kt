dbdf5a9b0802ba1694f5c6fe2bf1b371355c1ac2
NivinCNC <nivincharles@gmail.com>
2025-06-15 15:13:01 +0530

    cloudflare bypass

--- CNC Verse/src/main/kotlin/com/horis/cloudstreamplugins/PrimeVideoMirrorProvider.kt
+++ CNC Verse/src/main/kotlin/com/horis/cloudstreamplugins/PrimeVideoMirrorProvider.kt
@@ -31,6 +31,7 @@ class PrimeVideoMirrorProvider : MainAPI() {
     override val hasMainPage = true
     private var cookie_value = ""
     private val headers = mapOf(
+        "User-Agent" to "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
         "X-Requested-With" to "XMLHttpRequest"
     )
 
