87e09a7e6fd307177f0eda4a1e67f80d4e0c086a
NivinCNC <nivincharles@gmail.com>
2025-08-22 23:28:37 +0530

    aDDED GOLDENAUDIOBOOK

---
+++ GoldenAudiobook/src/main/kotlin/com/cncverse/GoldenAudiobookPlugin.kt
@@ -0,0 +1,11 @@
+package com.cncverse
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class GoldenAudiobookPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(GoldenAudiobook())
+    }
+}
